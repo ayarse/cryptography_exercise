4cedd6a286f48631ffd21fc2bb995746ac614972
Ayas Nasih <ayasnasih@gmail.com>
2022-06-21 02:47:04 +0500

    feat: add card issuer icons

---
+++ src/icons/index.ts
@@ -0,0 +1,9 @@
+/**
+ * Ayas Nasih - S1600655
+ * Villa College - BSCHCS (Jan 2020)
+ */
+import { ReactComponent as VisaIcon } from "./visa.svg";
+import { ReactComponent as AmexIcon } from "./amex.svg";
+import { ReactComponent as MastercardIcon } from "./mastercard.svg";
+
+export { VisaIcon, AmexIcon, MastercardIcon };
