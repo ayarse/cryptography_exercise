dc0a36573298ad418c330718d5e133ff13e9c34f
Ayas Nasih <ayasnasih@gmail.com>
2022-08-11 00:56:41 +0500

    fix: add React import

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@
  * Ayas Nasih - S1600655
  * Villa College - BSCHCS (Jan 2020)
  */
+import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
